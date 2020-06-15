8920f627fb8eb2e927c71dda3315ad68b2b10d50
benthornton1 <ben14367@hotmail.com>
2020-06-15 16:11:11 +0100

    uibuild

---
+++ part3/backend/build/precache-manifest.b014fce046445664e36d82b711aa20e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d368a8b5dbfff46ca7734d8daf1dabc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92eb500b5939030d67f1",
+    "url": "/static/css/main.987fbb6a.chunk.css"
+  },
+  {
+    "revision": "271a5e3958a7788d2011",
+    "url": "/static/js/2.befe412f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.befe412f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92eb500b5939030d67f1",
+    "url": "/static/js/main.501a1846.chunk.js"
+  },
+  {
+    "revision": "cbe2eeaabb8e794bb930",
+    "url": "/static/js/runtime-main.5ca970b0.js"
+  }
+]);
--- part3/backend/index.js
+++ part3/backend/index.js
@@ -11,6 +11,7 @@ const requestLogger = (req, res, next) => {
 }
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 app.use(requestLogger)
 
 let notes = [
@@ -33,10 +34,6 @@ let notes = [
       important: true
     }
 ]
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-})
 
 app.get('/api/notes', (req, res) => {
     res.json(notes)
